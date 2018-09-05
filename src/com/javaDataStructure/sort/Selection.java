c659350033eef40b190576b1258672dd0609d151
Jiangxs <songxjxs@yeah.ent>
2018-09-05 22:54:27 +0800

    排序

--- src/com/javaDataStructure/sort/Selection.java
+++ src/com/javaDataStructure/sort/Selection.java
@@ -5,16 +5,17 @@ public class Selection {
     public int[] selectionSort(int[] nums) {
 
         int length = nums.length;
-        int min;
+
         for (int i = 0; i < length; i++) {
-            min = i;
+            int min = i;
             for (int j = i + 1; j < length; j++) {
                 if (nums[min] > nums[j]) {
-                    int temp = min;
                     min = j;
-                    j = temp;
                 }
             }
+            int temp = nums[min];
+            nums[i] = nums[min];
+            nums[min] = temp;
         }
         return nums;
     }
--- src/com/javaDataStructure/sort/TestClient.java
+++ src/com/javaDataStructure/sort/TestClient.java
@@ -6,10 +6,10 @@ public class TestClient {
 
         int[] nums = {23,12,56,3,5,1,38,9,17,33,20,14,24,18};
         int[] arrays = {6, 1, 2, 7, 9, 3, 4, 5, 10, 8};
-//        Bubble bubble = new Bubble();
+        Bubble bubble = new Bubble();
 //        Selection selection = new Selection();
 //        Insertion insertion = new Insertion();
-//        printArray(bubble.bubbleSort(nums));
+        printArray(bubble.bubbleSort(nums));
 //        System.out.println();
 //        printArray(selection.selectionSort(nums));
 //        System.out.println();
@@ -22,10 +22,10 @@ public class TestClient {
 //        QuickSort quickSort = new QuickSort();
 //        printArray(quickSort.quickSort(arrays));
 
-        HeapSort heapSort = new HeapSort();
-        printArray(heapSort.sort(nums));
-        System.out.println();
-        System.out.println(nums[0]);
+//        HeapSort heapSort = new HeapSort();
+//        printArray(heapSort.sort(nums));
+//        System.out.println();
+//        System.out.println(nums[0]);
     }
 
     public static void printArray(int[] numbers){
